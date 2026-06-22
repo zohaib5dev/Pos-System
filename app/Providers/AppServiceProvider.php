1b9a809fc50447db9a67009a6612c17cfbf795f7
zohaib5laravel-code <zohaib5laravel@gmail.com>
2026-06-22 10:05:54 +0500

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+         if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
